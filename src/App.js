a9efc1fd4ee9c5d1477b9e5b58a09e0125aaa920
Huang <h96716@gmail.com>
2024-05-06 16:45:53 +0800

    feat: add signup api funciton to signup page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from "@/components/Layouts/Header/Header";
 import Footer from "@/components/Layouts/Footer/Footer";
-import { HomePage, LoginPage } from "@/pages/index";
+import { HomePage, LoginPage, SignUpPage } from "@/pages/index";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -10,9 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="login" element={<LoginPage />} />
-          {/* 
           <Route path="signup" element={<SignUpPage />} />
-         */}
           <Route path="*" element={<HomePage />} />
         </Routes>
       </BrowserRouter>
